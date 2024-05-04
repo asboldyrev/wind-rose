849d8779064e5186ed0713f06afd038774f98c18
asboldyrev <angelcorpc@ya.ru>
2024-05-04 12:04:51 +0700

    Добавил футер

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Wind rose</title>
+	<title>Роза ветров</title>
 
 	@vite(['resources/scss/style.scss', 'resources/js/app.js'])
 
@@ -15,6 +15,16 @@
 	<div class="container-fluid">
 		<div id="app"></div>
 	</div>
+	<footer class="bg-body-tertiary">
+		<div class="row gx-0 p-3">
+			<div class="col">
+				Исторические данные о погоде взяты с сервиса: <a target="_blank" href="https://open-meteo.com/">Open-Meteo</a>
+			</div>
+			<div class="col text-end">
+				<a href="https://pay.cloudtips.ru/p/99b3202e" target="_blank">Поблагодарить автора</a>
+			</div>
+		</div>
+	</footer>
 </body>
 
 </html>
