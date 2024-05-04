08978130dfcbbe67d625f08d3e69799753c8d153
asboldyrev <angelcorpc@ya.ru>
2024-05-04 12:33:09 +0700

    Добавил условие для кода метрики по окружению

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -28,27 +28,29 @@
 			</div>
 		</div>
 	</footer>
-	<!-- Yandex.Metrika counter -->
-	<script type="text/javascript">
-		(function(m, e, t, r, i, k, a) {
-			m[i] = m[i] || function() {
-				(m[i].a = m[i].a || []).push(arguments)
-			};
-			m[i].l = 1 * new Date();
-			for (var j = 0; j < document.scripts.length; j++) {
-				if (document.scripts[j].src === r) {
-					return;
+	@if (app()->environment('production'))
+		<!-- Yandex.Metrika counter -->
+		<script type="text/javascript">
+			(function(m, e, t, r, i, k, a) {
+				m[i] = m[i] || function() {
+					(m[i].a = m[i].a || []).push(arguments)
+				};
+				m[i].l = 1 * new Date();
+				for (var j = 0; j < document.scripts.length; j++) {
+					if (document.scripts[j].src === r) {
+						return;
+					}
 				}
-			}
-			k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-		})
-		(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-		document.ym = ym
-	</script>
-	<noscript>
-		<div><img src="https://mc.yandex.ru/watch/97196096" style="position:absolute; left:-9999px;" alt="" /></div>
-	</noscript>
-	<!-- /Yandex.Metrika counter -->
+				k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+			})
+			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+			document.ym = ym
+		</script>
+		<noscript>
+			<div><img src="https://mc.yandex.ru/watch/97196096" style="position:absolute; left:-9999px;" alt="" /></div>
+		</noscript>
+		<!-- /Yandex.Metrika counter -->
+	@endif
 </body>
 
 </html>
